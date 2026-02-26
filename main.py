b034381c4ca14c657249a3fb2f16e83027a73ece
Zaqazuchun <Bahaburgerbot@gmail.com>
2026-02-26 11:49:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,21 @@ from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 from database import init_db
 
-# handlers import (to‘g‘ri usul)
 import handlers.admin
 import handlers.products
 import handlers.orders
 
-
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
-
-# register handlers
 handlers.admin.register(dp)
 handlers.products.register(dp)
 handlers.orders.register(dp)
-
 
 async def main():
     await init_db()
     print("🚀 BOT STARTED")
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
