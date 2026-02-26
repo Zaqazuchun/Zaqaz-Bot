6601bf872b4f4e1dc8dbfee7413087f4160acdaa
Zaqazuchun <Bahaburgerbot@gmail.com>
2026-02-26 11:09:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 from database import init_db
 
+
 # handlers import
 from handlers import admin, products, orders
+from handlers import admin, products, orders
+
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
+
 # register handlers
 admin.register(dp)
+products.register(dp)
+orders.register(dp)
 products.register(dp)
 orders.register(dp)
 
