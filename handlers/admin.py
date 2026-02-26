5e5f3df6e051941c4fb7838da26ca9e9f2c89e5c
Zaqazuchun <Bahaburgerbot@gmail.com>
2026-02-26 11:17:03 +0500

    Create handlers/admin.py

---
+++ handlers/admin.py
@@ -0,0 +1,24 @@
+from aiogram import F
+from aiogram.types import Message
+from config import ADMIN_ID
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+def admin_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📦 Mahsulotlar")],
+            [KeyboardButton(text="🧾 Buyurtmalar")]
+        ],
+        resize_keyboard=True
+    )
+
+
+def register(dp):
+
+    @dp.message(F.text == "/start")
+    async def start_handler(message: Message):
+        if message.from_user.id == ADMIN_ID:
+            await message.answer("👨‍💼 Admin panel", reply_markup=admin_menu())
+        else:
+            await message.answer("🍔 Xush kelibsiz!\nMini App orqali buyurtma bering.")
